e89c637c9c56f269eef47930049ba60691f0364a
Maycon-MB <mayconbruno.constancio@gmail.com>
2025-02-12 18:18:23 -0300

    ajustando código

--- gui/consultar_curriculo.py
+++ gui/consultar_curriculo.py
@@ -1,6 +1,6 @@
 from PySide6.QtWidgets import (
     QWidget, QVBoxLayout, QHBoxLayout, QPushButton, QLabel, QLineEdit, QGridLayout,
-    QTableWidget, QTableWidgetItem, QComboBox, QSpinBox, QMessageBox, QScrollArea, QFileDialog, QSpacerItem
+    QTableWidget, QTableWidgetItem, QComboBox, QSpinBox, QMessageBox, QScrollArea, QFileDialog
 )
 from PySide6.QtCore import Qt
 from PySide6.QtGui import QIcon
@@ -27,6 +27,22 @@ class ConsultaWidget(QWidget):
         self.setWindowTitle("CONSULTAR CURRÍCULOS")
         self.setup_ui()
         self.search_curriculos()
+        
+        filter_layout = self.create_filter_layout()
+
+        # Setup do filtro de CPF
+        self.cpf_input = QLineEdit()
+        self.cpf_input.setPlaceholderText("Digite o CPF")
+        self.cpf_input.setInputMask("999.999.999-99")  # Máscara de CPF: 000.000.000-00
+        self.cpf_input.setFixedHeight(30)
+        filter_layout.addWidget(self.cpf_input, 0, 0)
+
+        # Setup do filtro de Telefone (Celular)
+        self.telefone_input = QLineEdit()
+        self.telefone_input.setPlaceholderText("Digite o Telefone")
+        self.telefone_input.setInputMask("(99) 99999-9999")  # Máscara para celular: (00) 00000-0000
+        self.telefone_input.setFixedHeight(30)
+        filter_layout.addWidget(self.telefone_input, 0, 1)
 
     def setup_ui(self):
         layout = QVBoxLayout()
@@ -34,6 +50,16 @@ class ConsultaWidget(QWidget):
         # Filtros de busca
         filter_layout = self.create_filter_layout()
         layout.addLayout(filter_layout)
+
+        # Converte os textos dos campos para maiúsculas
+        self.funcao_input.lineEdit().textChanged.connect(self.to_uppercase)
+        self.pcd_input.lineEdit().textChanged.connect(self.to_uppercase)
+        self.cep_input.textChanged.connect(self.to_uppercase)
+        self.sexo_input.lineEdit().textChanged.connect(self.to_uppercase)
+        self.servico_input.lineEdit().textChanged.connect(self.to_uppercase)
+        self.ctps_input.lineEdit().textChanged.connect(self.to_uppercase)
+        self.cidade_input.lineEdit().textChanged.connect(self.to_uppercase)
+        self.escolaridade_input.lineEdit().textChanged.connect(self.to_uppercase)
 
         # Botões de busca e limpeza
         button_layout = QHBoxLayout()
@@ -118,6 +144,12 @@ class ConsultaWidget(QWidget):
 
         self.setLayout(layout)
 
+    def to_uppercase(self):
+        sender = self.sender()
+        if sender:
+            sender.setText(sender.text().upper())
+
+
     def atualizar_cidade_com_cep(self):
         # Pega o texto do campo de CEP
         cep = self.cep_input.text()
@@ -132,45 +164,105 @@ class ConsultaWidget(QWidget):
     def create_filter_layout(self):
         filter_layout = QGridLayout()
 
-        # **Primeira coluna (SEXO, IDADE MÍNIMA, IDADE MÁXIMA)**
-        filter_layout.addWidget(QLabel("SEXO:"), 0, 0)
-        self.sexo_input = QComboBox()
-        self.sexo_input.setEditable(True)
-        self.sexo_input.addItems(["", "MASCULINO", "FEMININO"])
-        self.sexo_input.setFixedHeight(30)
-        filter_layout.addWidget(self.sexo_input, 0, 1)
+        # **Primeira linha (Função, PCD, Idade Mínima, CEP, Exp. Anos Min, Exp. Meses Min)**
+        filter_layout.addWidget(QLabel("FUNÇÃO:"), 0, 0)
+        self.funcao_input = QComboBox()
+        self.funcao_input.setEditable(True)
+        self.funcao_input.setFixedHeight(30)
+        
+        # Preencher o QComboBox com as funções da tabela
+        funcoes = self.curriculo_model.listar_funcao()
+        self.funcao_input.addItem("")  # Item vazio para filtrar todas as funções
+        self.funcao_input.addItems(funcoes)
+        filter_layout.addWidget(self.funcao_input, 0, 1)
 
-        filter_layout.addWidget(QLabel("IDADE MÍNIMA:"), 1, 0)
+        filter_layout.addWidget(QLabel("PCD:"), 0, 2)
+        self.pcd_input = QComboBox()
+        self.pcd_input.setEditable(True)
+        self.pcd_input.addItems(["", "Sim", "Não"])
+        self.pcd_input.setFixedHeight(30)
+        self.pcd_input.setFixedWidth(70)
+        filter_layout.addWidget(self.pcd_input, 0, 3)
+
+        filter_layout.addWidget(QLabel("IDADE MÍNIMA:"), 0, 4)
         self.idade_min_input = QSpinBox()
         self.idade_min_input.setRange(0, 120)
         self.idade_min_input.setSuffix(" anos")
         self.idade_min_input.setFixedHeight(30)
-        filter_layout.addWidget(self.idade_min_input, 1, 1)
+        filter_layout.addWidget(self.idade_min_input, 0, 5)
 
-        filter_layout.addWidget(QLabel("IDADE MÁXIMA:"), 2, 0)
+        filter_layout.addWidget(QLabel("CEP:"), 0, 6)
+        self.cep_input = QLineEdit()
+        self.cep_input.setPlaceholderText("Digite o CEP")
+        self.cep_input.setFixedHeight(30)
+        filter_layout.addWidget(self.cep_input, 0, 7)
+
+        filter_layout.addWidget(QLabel("EXP ANOS MIN:"), 0, 8)
+        self.experiencia_anos_min = QSpinBox()
+        self.experiencia_anos_min.setRange(0, 50)
+        self.experiencia_anos_min.setFixedHeight(30)
+        filter_layout.addWidget(self.experiencia_anos_min, 0, 9)
+
+        filter_layout.addWidget(QLabel("EXP MESES MIN:"), 0, 10)
+        self.experiencia_meses_min = QSpinBox()
+        self.experiencia_meses_min.setRange(0, 11)
+        self.experiencia_meses_min.setFixedHeight(30)
+        filter_layout.addWidget(self.experiencia_meses_min, 0, 11)
+
+        # **Segunda linha (Serviço, CTPS, Idade Máxima, Cidade, Exp. Anos Max, Exp. Meses Max)**
+        filter_layout.addWidget(QLabel("SERVIÇO:"), 1, 0)
+        self.servico_input = QComboBox()
+        self.servico_input.setEditable(True)
+        self.servico_input.addItems(["", "SINE", "MANUAL"])
+        self.servico_input.setFixedHeight(30)
+        filter_layout.addWidget(self.servico_input, 1, 1)
+
+        filter_layout.addWidget(QLabel("CTPS:"), 1, 2)
+        self.ctps_input = QComboBox()
+        self.ctps_input.setEditable(True)
+        self.ctps_input.addItems(["", "Sim", "Não"])
+        self.ctps_input.setFixedHeight(30)
+        self.ctps_input.setFixedWidth(70)
+        filter_layout.addWidget(self.ctps_input, 1, 3)
+
+        filter_layout.addWidget(QLabel("IDADE MÁXIMA:"), 1, 4)
         self.idade_max_input = QSpinBox()
         self.idade_max_input.setRange(0, 120)
         self.idade_max_input.setSuffix(" anos")
         self.idade_max_input.setFixedHeight(30)
-        filter_layout.addWidget(self.idade_max_input, 2, 1)
+        filter_layout.addWidget(self.idade_max_input, 1, 5)
 
-        # **Segunda coluna (CEP, CIDADE, ESCOLARIDADE)**
-        filter_layout.addWidget(QLabel("CEP:"), 0, 2)
-        self.cep_input = QLineEdit()
-        self.cep_input.setPlaceholderText("Digite o CEP")
-        self.cep_input.setFixedHeight(30)
-        filter_layout.addWidget(self.cep_input, 0, 3)
-
-        filter_layout.addWidget(QLabel("CIDADE:"), 1, 2)
+        filter_layout.addWidget(QLabel("CIDADE:"), 1, 6)
         self.cidade_input = QComboBox()
         self.cidade_input.setEditable(True)
         self.cidade_input.addItem("")
         cidades = self.curriculo_model.listar_cidades()
         self.cidade_input.addItems(cidades)
         self.cidade_input.setFixedHeight(30)
-        filter_layout.addWidget(self.cidade_input, 1, 3)
+        filter_layout.addWidget(self.cidade_input, 1, 7)
 
-        filter_layout.addWidget(QLabel("ESCOLARIDADE:"), 2, 2)
+        filter_layout.addWidget(QLabel("EXP ANOS MAX:"), 1, 8)
+        self.experiencia_anos_max = QSpinBox()
+        self.experiencia_anos_max.setRange(0, 50)
+        self.experiencia_anos_max.setFixedHeight(30)
+        filter_layout.addWidget(self.experiencia_anos_max, 1, 9)
+
+        filter_layout.addWidget(QLabel("EXP MESES MAX:"), 1, 10)
+        self.experiencia_meses_max = QSpinBox()
+        self.experiencia_meses_max.setRange(0, 11)
+        self.experiencia_meses_max.setFixedHeight(30)
+        filter_layout.addWidget(self.experiencia_meses_max, 1, 11)
+
+        # **Terceira linha (Sexo)**
+        filter_layout.addWidget(QLabel("SEXO:"), 2, 0)
+        self.sexo_input = QComboBox()
+        self.sexo_input.setEditable(True)
+        self.sexo_input.addItems(["", "MASCULINO", "FEMININO"])
+        self.sexo_input.setFixedHeight(30)
+        filter_layout.addWidget(self.sexo_input, 2, 1)
+
+        # **Quarta linha (Escolaridade abaixo de CTPS)**
+        filter_layout.addWidget(QLabel("ESCOLARIDADE:"), 2, 2)  # Coluna 2, linha 2
         self.escolaridade_input = QComboBox()
         self.escolaridade_input.setEditable(True)
         self.escolaridade_input.addItems([
@@ -186,60 +278,7 @@ class ConsultaWidget(QWidget):
             "DOUTORADO"
         ])
         self.escolaridade_input.setFixedHeight(30)
-        filter_layout.addWidget(self.escolaridade_input, 2, 3)
-
-        # **Terceira coluna (FUNÇÃO, SERVIÇO, PCD)**
-        filter_layout.addWidget(QLabel("FUNÇÃO:"), 0, 4)
-        self.funcao_input = QLineEdit()
-        self.funcao_input.setPlaceholderText("Digite a função")
-        self.funcao_input.setFixedHeight(30)
-        filter_layout.addWidget(self.funcao_input, 0, 5)
-
-        filter_layout.addWidget(QLabel("SERVIÇO:"), 1, 4)
-        self.servico_input = QComboBox()
-        self.servico_input.setEditable(True)
-        self.servico_input.addItems(["", "SINE", "MANUAL"])
-        self.servico_input.setFixedHeight(30)
-        filter_layout.addWidget(self.servico_input, 1, 5)
-
-        filter_layout.addWidget(QLabel("PCD:"), 2, 4)
-        self.pcd_input = QComboBox()
-        self.pcd_input.setEditable(True)
-        self.pcd_input.addItems(["", "Sim", "Não"])
-        self.pcd_input.setFixedHeight(30)
-        filter_layout.addWidget(self.pcd_input, 2, 5)
-
-        # **Quarta coluna (EXPERIÊNCIA, CTPS)**
-        filter_layout.addWidget(QLabel("EXPERIÊNCIA (ANOS MÍN.):"), 0, 6)
-        self.experiencia_anos_min = QSpinBox()
-        self.experiencia_anos_min.setRange(0, 50)
-        self.experiencia_anos_min.setFixedHeight(30)
-        filter_layout.addWidget(self.experiencia_anos_min, 0, 7)
-
-        filter_layout.addWidget(QLabel("EXPERIÊNCIA (ANOS MÁX.):"), 1, 6)
-        self.experiencia_anos_max = QSpinBox()
-        self.experiencia_anos_max.setRange(0, 50)
-        self.experiencia_anos_max.setFixedHeight(30)
-        filter_layout.addWidget(self.experiencia_anos_max, 1, 7)
-
-        filter_layout.addWidget(QLabel("EXPERIÊNCIA (MESES MÍN.):"), 2, 6)
-        self.experiencia_meses_min = QSpinBox()
-        self.experiencia_meses_min.setRange(0, 11)
-        self.experiencia_meses_min.setFixedHeight(30)
-        filter_layout.addWidget(self.experiencia_meses_min, 2, 7)
-
-        filter_layout.addWidget(QLabel("EXPERIÊNCIA (MESES MÁX.):"), 3, 6)
-        self.experiencia_meses_max = QSpinBox()
-        self.experiencia_meses_max.setRange(0, 11)
-        self.experiencia_meses_max.setFixedHeight(30)
-        filter_layout.addWidget(self.experiencia_meses_max, 3, 7)
-
-        filter_layout.addWidget(QLabel("CTPS:"), 4, 6)
-        self.ctps_input = QComboBox()
-        self.ctps_input.setEditable(True)
-        self.ctps_input.addItems(["", "Sim", "Não"])
-        self.ctps_input.setFixedHeight(30)
-        filter_layout.addWidget(self.ctps_input, 4, 7)
+        filter_layout.addWidget(self.escolaridade_input, 2, 3)  # Coloca a combo box ao lado do campo "SEXO"
 
         return filter_layout
 
@@ -289,7 +328,7 @@ class ConsultaWidget(QWidget):
                 "Sim": True,
                 "Não": False
             }.get(self.ctps_input.currentText(), None),
-            "funcao": self.funcao_input.text().strip() or None,
+            "funcao": self.funcao_input.currentText() or None,  # Agora o filtro de função é tratado aqui
             "experiencia_min": experiencia_min_meses if experiencia_min_meses > 0 else None,
             "experiencia_max": experiencia_max_meses if experiencia_max_meses > 0 else None,
         }
@@ -313,13 +352,25 @@ class ConsultaWidget(QWidget):
         """
         self.table.setRowCount(len(results))
         for row_idx, row in enumerate(results):
+            # Aplicar a máscara de CPF
+            cpf = row.get("cpf", "")
+            formatted_cpf = self.format_cpf(cpf)
+            
+            # Aplicar a máscara de telefone
+            telefone = row.get("telefone", "")
+            formatted_telefone = self.format_telefone(telefone)
+            
+            # Aplicar a máscara de telefone extra
+            telefone_extra = row.get("telefone_extra", "")
+            formatted_telefone_extra = self.format_telefone(telefone_extra)
+
             # Mapeia os dados para as colunas da tabela
             data = [
-                row.get("cpf", ""),
+                formatted_cpf,
                 row.get("nome", ""),
                 row.get("idade", ""),
-                row.get("telefone", ""),
-                row.get("telefone_extra", ""),
+                formatted_telefone,
+                formatted_telefone_extra,
                 row.get("cep", ""),
                 row.get("cidade", ""),
                 row.get("escolaridade", ""),
@@ -340,10 +391,18 @@ class ConsultaWidget(QWidget):
             edit_button = QPushButton("EDITAR")
             edit_button.clicked.connect(lambda _, id=row["curriculo_id"]: self.open_edit_dialog(id))
             self.table.setCellWidget(row_idx, 13, edit_button)  # Última coluna é a de AÇÕES (índice 13)
-
+        
         # Atualiza o total de resultados exibido
         self.total_label.setText(f"TOTAL DE CURRÍCULOS: {self.total_results}")
 
+    def format_cpf(self, cpf):
+        """Formata CPF para o padrão XXX.XXX.XXX-XX"""
+        return f"{cpf[:3]}.{cpf[3:6]}.{cpf[6:9]}-{cpf[9:]}"
+
+    def format_telefone(self, telefone):
+        """Formata o telefone para o padrão (XX) XXXXX-XXXX"""
+        return f"({telefone[:2]}) {telefone[2:7]}-{telefone[7:]}"
+    
     def update_pagination_controls(self):
         """
         Atualiza os botões de paginação e o rótulo de página.
@@ -431,37 +490,49 @@ class ConsultaWidget(QWidget):
         c.drawString(margin, y_position, "Relatório de Currículos Consultados")
         y_position -= 20
 
-        # Cabeçalho da tabela
-        c.setFont("Helvetica-Bold", 10)
-        header = [
-            "CPF", "NOME", "IDADE", "TELEFONE", "TELEFONE EXTRA", "CEP",
-            "CIDADE", "ESCOLARIDADE", "FUNÇÃO", "ANOS EXP.", "MESES EXP.",
-            "CTPS", "PCD"
-        ]
-        col_widths = [50, 100, 40, 80, 80, 60, 100, 120, 100, 50, 50, 60, 50]
-
-        for col_idx, header_text in enumerate(header):
-            c.drawString(margin + sum(col_widths[:col_idx]), y_position, header_text)
-
-        y_position -= 20
+        # Definir a largura das colunas
+        num_columns = 3
+        col_width = (width - 2 * margin) / num_columns
+        x_positions = [margin + i * col_width for i in range(num_columns)]
 
         # Dados da tabela
         c.setFont("Helvetica", 10)
+        line_height = 15  # Altura de uma linha de dados
+        max_y_position = 40  # Posição mínima antes de ir para nova página
+
+        # Desenhando cada linha da tabela
         for row in rows:
-            for col_idx, cell_text in enumerate(row):
-                c.drawString(margin + sum(col_widths[:col_idx]), y_position, str(cell_text))
-            y_position -= 15
+            col1 = row[1]  # Nome (alinhado à esquerda)
+            col2 = row[5]  # CEP (centralizado)
+            col3 = row[3]  # Telefone (alinhado à direita)
+            col3 = self.format_telefone(col3)  # Formata o telefone no PDF
 
-            # Verificar se precisa de nova página
-            if y_position < 40:
-                c.showPage()
+            col4 = row[0]  # CPF (alinhado à esquerda)
+            col4 = self.format_cpf(col4)  # Formata o CPF no PDF
+            col5 = row[6]  # Cidade (centralizado)
+            col6 = row[4]  # Telefone Extra (alinhado à direita)
+            col6 = self.format_telefone(col6)  # Formata o telefone extra no PDF
+
+            col7 = f"{row[2]} anos"  # Idade (alinhado à esquerda)
+            col8 = row[7]  # Escolaridade (centralizado)
+            col9 = "PCD" if row[12] == "Sim" else "Não é PCD"  # PCD (alinhado à direita)
+
+            # Verificar se há espaço suficiente para a linha de dados
+            if y_position - line_height < max_y_position:
+                c.showPage()  # Adiciona uma nova página
                 c.setFont("Helvetica", 10)
-                y_position = height - 40
-                for col_idx, header_text in enumerate(header):
-                    c.drawString(margin + sum(col_widths[:col_idx]), y_position, header_text)
-                y_position -= 20
+                y_position = height - 40  # Reinicia a posição no topo da nova página
 
-        # Salvar o PDF
+                # Reescrever o título após criar uma nova página
+                c.drawString(margin, y_position, "Relatório de Currículos Consultados")
+                y_position -= 20  # Ajusta a posição do título
+
+            # Escreve as colunas no PDF
+            c.drawString(x_positions[0], y_position, col1)
+            c.drawString(x_positions[1], y_position, col2)
+            c.drawString(x_positions[2], y_position, col3)
+            y_position -= line_height
+
         c.save()
 
         # Notificar o usuário
